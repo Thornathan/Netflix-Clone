3b0eb60095a85bc69c1e63c0378e5fbca3b42597
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-10-22 00:36:48 -0700

    Completed Rows for API get

--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import "./Row.css"
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -17,15 +17,15 @@ function Row({ title, fetchUrl }) {
   }, [fetchUrl]);
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
 
       <div className="row_posters">
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className="row_poster"
-            src={`${base_url}${movie.poster_path}`}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
             alt={movie.name}
           />
         ))}
