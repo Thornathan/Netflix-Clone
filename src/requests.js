41bd835c06543a7a894719ac54907d9cfa1c1904
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-10-21 18:05:53 -0700

    created api requests

--- src/requests.js
+++ src/requests.js
@@ -1,1 +1,14 @@
-const APIKEY = "79a1b34ac09a15b33c72bd9a1241edc2";
+const API_KEY = "79a1b34ac09a15b33c72bd9a1241edc2";
+
+const requests =  {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movies/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
